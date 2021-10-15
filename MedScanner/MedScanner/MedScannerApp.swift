cf1e9cb62989bf8594953ce1721967f57dd6d31b
Giovanni Palusa <giovanni@palusa.se>
2021-10-15 09:50:27 +0200

    Starting project

---
+++ MedScanner/MedScanner/MedScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedScannerApp.swift
+//  MedScanner
+//
+//  Created by Giovanni Palusa on 2021-10-15.
+//
+
+import SwiftUI
+
+@main
+struct MedScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
